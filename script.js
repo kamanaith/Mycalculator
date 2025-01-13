1ebade8a1f49ed14845e3be7b25efde7bdd4a8e8
kamanaith <timsinakamaksha@gmail.com>
2025-01-13 09:43:23 +0545

    js updated

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
 let currentInput = '';
 
-let priviousInput = '';
+let previousInput = '';
 
 let operator = '';
 
-function appendNumber(number){
+function appendNumber(number) {
     if (number === '.' && currentInput.includes('.')) return;
     currentInput += number;
     updateDisplay();
 }
-function appendOperator(operator){
+function appendOperator(operator) {
     if(currentInput === '') return;
     if(previousInput !== '') calculate();
     previousInput = currentInput;
     currentInput = '';
     updateDisplay();
 }
+function updateDisplay() {
+    const display = document.getElementsByClassName('display');
+    display.innerText = currentInput || previousInput || '0';   
+}
 function clearDisplay() {
     currentInput = '';
-    priviousInput = '';
-    
+    previousInput = '';
+    operator = '';
+}
+function deleteDigit() {
+    currentInput = currentInput.slice(0,-1);
+    updateDisplay();
+}
+function calculate() {
+    if(currentInput === '' || previousInput === '') return;
+    const prev = parseFloat(previousInput);
+    const cur = parseFloat(currentInput);
+
+    switch(operator)
+    {
+        case '+':
+            currentInput = prev + cur ;
+            break;
+        case '-':
+            currentInput = prev - cur ;
+            break;
+        case '*':
+            currentInput = prev * cur ;
+            break;
+        case '/':
+            currentInput = cur === 0 ? 'error' : prev/cur;           
+            break;
+        case '%':
+            currentInput = prev % cur;    
+            break;
+        default:
+            return;    
+    }
+
 }
