643b54380242fe973638fa1607ac8236a9f1f336
kamanaith <timsinakamaksha@gmail.com>
2025-02-08 19:44:05 +0545

    logic updated

--- script.js
+++ script.js
@@ -1,104 +1,113 @@
-// let currentInput = document.querySelector(".currentInput");
-// let currentInput = 0;
-
-// let previousInput = document.querySelector(".previousInput");
-// let previousInput = 0;
-
 let display = document.querySelector(".display");
 
-let result = "";
-
-// console.log(previousInput);
-let number = "";
-let operator;
+let num = [0,1,2,3,4,5,6,7,8,9];
+let oper = ["%","/","*","-","+","."];
+let result;
 let entries = [];
+let number;
+let operator;
 
-function reset() {
-  number = "";
-  operator = "";
-  entries = [];
-}
-
-function appendNumber(num) { 
-
- if (number.length > 0) {
-  number +=num;
- } else {
-  number = num;
- }
-  console.log("number entered");
- 
-  display.textContent += number;
-
-}
-
-function appendOperator(op) { 
-
-  console.log("operator entered");
-  operator = op;
-  display.textContent += operator;
-  number = Number(number);
-
-  entries.push(number);
- number = "";
+function reset(){
+  display.textContent = "";
 }
 
 function clearButton() {
   console.log("clear all");
   display.textContent = reset();
+  console.log("cleared");
   return;
 }
 
 function deleteButton() {
   console.log("back");
-display.textContent = display.textContent.substring(0,display.textContent.length-1);
-if (display.textContent.length = 0) {
-  display.textContent = reset();
-}
-
+  display.textContent = display.textContent.substring(
+    0,
+    display.textContent.length - 1
+  );
+  if ((display.textContent.length = 0)) {
+    display.textContent = reset();
+  }
   return;
 }
-function calculate() {
 
-  console.log(number);
+  function clicked(e) { 
+  if ( num.includes(e)) {
+    console.log("number clicked");
   
+     number = String(e);
+     number.concat(number);
+  console.log(typeof(number));
+  console.log("number=",number);
+  
+    display.textContent += number;
+    // entries.push(number);
+    console.log(entries);
+    
+    
+  } else if (oper.includes(e)) {
+    
+    number = Number(number);
+    console.log(typeof(number));
+    entries.push(number);
+    
+    let op;
+    console.log("operator clicked");
+    operator = op;
+    op = String(e);
+    console.log(typeof(op));
+    
+    display.textContent += op;
+    entries.push(op);
+    console.log(entries);
+    
+  }
+  }
+function calculate() {
   number = Number(number);
-  
+  console.log(typeof(number));
   entries.push(number);
-  console.log(entries);
-  number = "";
-  switch (operator) {
+  // entries.push(number);
+  // console.log(entries);
+  // number = "";
+  switch (entries[1]) {
     case "+":
-      result = entries[0] + entries[1];
+      result = entries[0] + entries[2] ;
+      console.log("result",result);
+      
       break;
     case "-":
-      result = entries[0] - entries[1];
+      result = entries[0] - entries[2];
       break;
     case "/":
-      if (entries[1] === 0) {
+      if (entries[2] === 0) {
         alert(console.log("cannot be divisible by zero"));
       } else {
-        result = entries[0] / entries[1];
+        result = entries[0] / entries[2];
       }
       break;
     case "*":
-      result = entries[0] * entries[1];
+      result = entries[0] * entries[2];
       break;
     case "%":
-      result = entries[0] % entries[1];
+      result = entries[0] % entries[2];
       break;
     default:
       result = "invalid";
       break;
   }
+
+  entries.push(result);
   display.textContent = result;
-  entries = [];
-  number = "";
+  
+  // display.textContent = result;
+
+  // number = "";
   console.log(entries);
+  entries = [];
   // console.log(typeof( number));
-  number += result;
+  // number += result;
   // result.textContent += number;
-  console.log(number);
+  // console.log(number);
   // entries.push(number);
   // result = String(result)
   // result.textContent = number;
